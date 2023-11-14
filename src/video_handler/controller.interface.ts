3d92e012eebd32e6dd2bcfba67af636c8f1ab927
Anna-Sofiia Ch <sofiya.dubl@gmail.com>
2023-11-14 11:12:26 -0800

    ME-3 Upload video function added to interface

--- src/video_handler/controller.interface.ts
+++ src/video_handler/controller.interface.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 
 export interface IVideoController {
+	uploadVideo(req: Request, res: Response, next: NextFunction): void;
 	getVideo(req: Request, res: Response, next: NextFunction): void;
 	giveVideo(req: Request, res: Response, next: NextFunction): void;
 }
